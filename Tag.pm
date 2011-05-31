f810afd806955c0e083d3b07ef1564c7a1034970
Conor Horan-Kates <conor.code@gmail.com>
2011-05-30 18:07:04 -0700

    Tag - initial commit to create directory structure

---
+++ Tag.pm
@@ -0,0 +1,11 @@
+package HTML::Tag;
+use strict;
+use warnings;
+
+require Exporter;
+our @ISA = qw(Exporter);
+our @EXPORT    = qw();
+our @EXPORT_OK = qw();
+
+## generalized routines for generating HTML tag clouds
+
