1fa12a7a4abf39da7699139878987ba330225a00
Conor Horan-Kates <conor.code@gmail.com>
2011-05-30 20:49:06 -0700

    Tag - initial working commit

--- HTML/Tag.pm
+++ HTML/Tag.pm
@@ -11,7 +11,8 @@ our @EXPORT_OK = qw(build_cloud);
 
 
 $HTML::Tag::Font_Default = 'Tahoma';
-@HTML::Tag::Font_Sizes = (5, 7, 9, 11, 13);
+#@HTML::Tag::Font_Sizes = (5, 7, 9, 11, 13);
+@HTML::Tag::Font_Sizes = ('70%', '85%', '100%', '115%', '130%'); # hmm..
 
 ## generalized routines for generating HTML tag clouds
 
@@ -26,72 +27,71 @@ sub build_cloud {
     my $html;
 
     ## need to cull entries with less than $min_count
+	
 
-    ## need to determine how many sizes to use -- max of 5
-    if (scalar(keys %tags) <= $#HTML::Tag::Font_Sizes) {
-	# each tag can have it's own size
+	my @ordered = sort { $tags{$a}{count} <=> $tags{$b}{count} } keys %tags;
+	my $high = $tags{$ordered[-1]}{count};
+	my $range = ($high / $#HTML::Tag::Font_Sizes);
 	
 	foreach my $tag (keys %tags) { 
-	    next if $tag =~ /^_/; # skipping internals
-	    my $count = $tags{$tag}{count};
+		next if $tag =~ /^_/; # skipping internals
+		my $count = $tags{$tag}{count};
 
-	    my $size = 10;
-
-	    $tags{$tag}{size} = $size;
+		my $size = ($count < ($range)) ? $HTML::Tag::Font_Sizes[0] :
+				   ($count < ($range * 2)) ? $HTML::Tag::Font_Sizes[1] :
+				   ($count < ($range * 3)) ? $HTML::Tag::Font_Sizes[2] :
+				   ($count < ($range * 4)) ? $HTML::Tag::Font_Sizes[3] :
+																	$HTML::Tag::Font_Sizes[-1]; # default
+		$tags{$tag}{size} = $size;
 
 	}
 
-
-    } else {
-	# need to determine ranges and dynamically place
-    }
-
     ## build the initial HTML
     my $id = 'tag_cloud_' . rand(10000);
-    $html = '<div id=' . $id . ' style="display: inline; border-style: solid; border-width: 1px; border-color: #000000; position: absolute; height: $height; width: $width;">';
+    $html = "<div id=\"$id\" style=\"display: inline; border-style: solid; border-width: 1px; border-color: #000000; position: absolute; height: $height; width: $width;\">";
 
     ## iterate keys according to sort
     my @keys;
     if ($sort_method =~ /value/i) { 
-	@keys = sort { $tags{$a}{count} <=> $tags{$b}{count} } keys %tags;
+		@keys = sort { $tags{$a}{count} <=> $tags{$b}{count} } keys %tags;
     } else {
-	@keys = sort { $a cmp $b } keys %tags;
+		@keys = sort { $a cmp $b } keys %tags;
     }
 
     for (my $i = 0; $i <= $#keys; $i++) { 
-	next if $keys[$i] =~ /^_/i; # skipping internal settings
-	my $lhtml;
-	my $key = $keys[$i];
-	my %tag = %{$tags{$key}}; # need to have some error checking here
-	my $size = $tag{size};
-	my $count = $tag{count};
-	my $base = $tags{_base};
-
-	my $link;
-	if (defined $tag{link_rel}) {
-	    my $link = $tag{link_rel};
-	    my $alt  = $count;
-	    
-	    warn "WARN:: '<base>' specified in '$key', but no '_base' spec in incoming hash" unless $base;
-	    $link =~ s/<base>/$base/;
-	    $link =~ s/<value>/$count/g;
-	    $link =~ s/<key>/$key/g;
-
-
-	    $lhtml = "<a href=$link title=$count><font size=$size>$key</font></a>";
-
-	} elsif (defined $tag{link_abs}) { 
-	    my $link = $tag{link_abs};
-	    my $alt  = $count;
-
-	    $lhtml = "<a href=$link title=$alt><font size=$size>$key</font></a>";
-
-	} else  {
-	    warn "WARN:: no 'link_abs' or 'link_rel' specified for '$key'";
-	    next;
-	}
-
-	$html .= " " . $lhtml;
+		next if $keys[$i] =~ /^_/i; # skipping internal settings
+		my $lhtml;
+		my $key = $keys[$i];
+		my %tag = %{$tags{$key}}; # need to have some error checking here
+		my $size = $tag{size};
+		my $count = $tag{count};
+		my $base = $tags{_internal}{base};
+	
+		my $link;
+		if (defined $tag{link_rel}) {
+			my $link = $tag{link_rel};
+			my $alt  = $count;
+			
+			warn "WARN:: '<base>' specified in '$key', but no '_base' spec in incoming hash" unless $base;
+			$link =~ s/<base>/$base/;
+			$link =~ s/<value>/$count/g;
+			$link =~ s/<key>/$key/g;
+	
+	
+			$lhtml = "<a href=\"$link\" title=\"$count\"><span style=\"font-size: $size;\">$key</span></a>";
+	
+		} elsif (defined $tag{link_abs}) { 
+			my $link = $tag{link_abs};
+			my $alt  = $count;
+	
+			$lhtml = "<a href=$link title=$alt><span style='font-size: $size'>$key</span></a>";
+	
+		} else  {
+			warn "WARN:: no 'link_abs' or 'link_rel' specified for '$key'";
+			next;
+		}
+	
+		$html .= " " . $lhtml;
 
     }
 
--- t/basic.t
+++ t/basic.t
@@ -4,7 +4,6 @@
 use strict;
 use warnings;
 
-use Test::HTML::W3C;
 use Test::More qw(no_plan);
 
 
@@ -18,24 +17,71 @@ my $out;
 
 %in = (
     foo => {
-	count => 42,
-	link_rel => '<base>?value=<value>',
+        count => 42,
+        link_rel => '<base>?value=<value>',
     },
     bar => {
-	count => 13,
-	link_abs => 'http://foo.com/foo.php?value=bar',
+        count => 13,
+        link_abs => 'http://foo.com/foo.php?value=bar',
     },
     baz => {
-	count => 5,
-	link_rel => '<base>?value=<value>',
+        count => 5,
+        link_rel => '<base>?value=<value>',
     },
-    '_base' => 'http://foo.com/foo.php',
-    );
+    '_internal' => {
+        'base' => 'http://foo.com/foo.php',
+        'count' => 0, # hacky
+    },
+    
+);
 
-$out = HTML::Tag::build_cloud(\%in, 0);
+$out = HTML::Tag::build_cloud(\%in, 50, 50, 0);
 
 is (defined $out, 1, "some content was returned:: $out");
 #is_valid_markup($out);
 
+%in = (
+    one => {
+        count => 1,
+        link_rel => '<base>?value=<value>',
+    },
+    two => {
+        count => 22,
+        link_abs => 'http://foo.com/foo.php?value=bar',
+    },
+    three => {
+        count => 44,
+        link_rel => '<base>?value=<value>',
+    },
+    four => {
+        count => 64,
+        link_rel => '<base>?value=<value>',
+    },
+    five => {
+        count => 84,
+        link_rel => '<base>?value=<value>',
+    },
+    '_internal' => {
+        'base' => 'http://foo.com/foo.php',
+        'count' => 0, # hacky
+    },
+    
+);
+
+$out = HTML::Tag::build_cloud(\%in, 100, 100, 0);
+
+is (defined $out, 1, "some content was returned:: $out");
+
+## build a 100 element hash
+%in = ( '_internal' => { 'base' => 'http://bar.com/bar.php', count => 0 } );
+for (my $i = 0; $i < 100; $i++) {
+    my $key = $i;
+    $in{$key}{count}    = $key;
+    $in{$key}{link_rel} = '<base>?value=<value>';
+}
+
+$out = HTML::Tag::build_cloud(\%in, 200, 200, 0);
+
+is (defined $out, 1, "some content was returned:: $out");
 
 exit;
