398047ea45124dc15b3500957defcad2b2c8bd39
Conor Horan-Kates <conor.code@gmail.com>
2011-06-04 14:33:34 -0700

    added POD documentation to the module

--- HTML/TagCloud/Simple.pm
+++ HTML/TagCloud/Simple.pm
@@ -2,12 +2,14 @@ package HTML::Tag;
 use strict;
 use warnings;
 
-use 5.010;
+use 5.010; # sorry, need smart matching
 
 require Exporter;
 our @ISA = qw(Exporter);
 our @EXPORT    = qw();
 our @EXPORT_OK = qw(build_cloud);
+
+our $VERSION = '0.01';
 
 ## TODO
 # need to add the reverse sorting mechanisms
@@ -107,3 +109,46 @@ sub build_cloud {
 
 
 1;
+
+__END__
+
+=head1 NAME
+    HTML::TagCloud::Simple - Generate an HTML Tag cloud (simple interface)
+
+=head1 SYNOPSIS
+
+    HTML::TagCloud::Simple is a very simple module that allows you to easily build tag clouds, similar to what delicious.com, flickr.com and many other websites provide (ex L<http://www.delicious.com/tag/>)
+
+    To use:
+
+    #!/usr/bin/perl -w
+
+    use HTML::TagCloud::Simple;
+
+my %h = ( foo => { count => 5, link_rel => '?count=<count>&value=<value>' }, bar => { count => 5, link_abs => 'http://bar.com' } );
+
+my $html = HTML::TagCloud::Simple::build_cloud(\%h, 'http://baz.com/foo.php', 100, 100);
+
+...
+
+    For more examples, see t/*.t
+
+
+=head1 DESCRIPTION
+
+    Initial implementation has only one function:
+
+     build_cloud(\%hash, $base_url, $height, $width, [$sort_method], [$min_count]) -- returns an HTML string
+
+     $height/$width - because this function returns a <div> with the tag inside, it needs to know how big to make the <div>
+    $base_url- will be concatenated to to $hash{link_rel} to create an FQDN
+     $sort_method   - can be 'ascii', 'value', 'reverse-ascii' or 'reverse-value'
+    $min_count     - if $hash{count} < this, the link will not be included in the tag
+
+=head1 AUTHOR
+
+    Conor Horan-Kates, "<conor@cpan.org>".
+
+=head1 COPYRIGHT
+
+    This module is free software; you can redistribute it or modify it under the same terms as Perl itself.
